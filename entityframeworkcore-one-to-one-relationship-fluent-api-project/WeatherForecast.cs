57f693224f8be52a22fddfa145af55eb06b005c9
teodorp11 <teodor.posea@gmail.com>
2025-11-21 23:29:14 +0200

    Commit 1: Initial commit (Initial ASP.NET Core Web API Project)

---
+++ entityframeworkcore-one-to-one-relationship-fluent-api-project/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace entityframeworkcore_one_to_one_relationship_fluent_api_project
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
